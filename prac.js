795738ec850dc8017646a767f0d6565aa26f4eca
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2024-01-12 10:01:19 +0530

    .

--- prac.js
+++ prac.js
@@ -18,3 +18,20 @@
 // for(let i = 1 ; i<=100 ;i++){
 //     console.log(sum = sum+i)  
 // } 
+
+
+// ARRAYS IN JS
+//The most simplest way of aggregating data is by using array 
+//example --> const ages = {1,4,2}.... etc
+//printing the even numbers in array ages
+// const ages = [22,26,25];
+// for(let i = 0 ;i<=ages.length ; i++){
+//     if (ages[i] % 2 ==0){
+//         console.log(ages[i]);
+//     }
+//     else{
+//         console.log("Not Found");
+//     }
+// }
+
+
