65ed5ebf0e65195c3f491462ede9fa88d00e0ac0
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2024-01-12 09:47:29 +0530

    .

--- prac.js
+++ prac.js
@@ -1,5 +1,20 @@
+// Basics of Javascript
+
 // let firstName = "Ashu";
 // let secondName = "Pandey";
 
 // console.log("Hello namaste" + firstName + secondName + " How are u doing today " );
 
+//Loops
+
+//print a umber from 1 to 100
+// let n;
+// for(let i=1 ;  i<=100 ; i++ ){
+//     console.log(i);
+// }  
+
+// TO print the sum of the numbers from 1 to 100
+// let sum = 0;
+// for(let i = 1 ; i<=100 ;i++){
+//     console.log(sum = sum+i)  
+// } 
