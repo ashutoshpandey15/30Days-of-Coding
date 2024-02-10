2a2d4232611195eec0aba1b825c957c589e66304
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2024-02-10 17:17:29 +0530

    added tuples

--- p1.py
+++ p1.py
@@ -14,9 +14,13 @@ print(str1[::-1])
 #LIST
 #list is a datatype in python which can hold multiple data types.
 #It's like an array or tuple but mutable.
-l1 = [1,2,3,4,5,6,7,8,9,10]
+"""l1 = [1,2,3,4,5,6,7,8,9,10]
 print(l1)
 l1.remove(5)
 print(l1)
-
-
+"""
+#Tuples
+#Tuples are immutable just like strings.
+#Tuples are used to store multiple items in a single variable.
+tup = (1,2,3,"ashu",True)
+print(tup)
