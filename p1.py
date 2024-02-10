6572937a8b7dd7c041f66edfb1500f5e258164ad
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2024-02-10 17:14:36 +0530

    .

--- p1.py
+++ p1.py
@@ -6,8 +6,17 @@ print(type(a))
 print(type(b))
 
 '''
-str1 = "Ashutosh"
+'''str1 = "Ashutosh"
 abc = str1[:0]
 print(str1[::-1])
-name = input("Enter name :")
-print("Your name is : ", name)
+'''
+#Strings are immutable in python
+#LIST
+#list is a datatype in python which can hold multiple data types.
+#It's like an array or tuple but mutable.
+l1 = [1,2,3,4,5,6,7,8,9,10]
+print(l1)
+l1.remove(5)
+print(l1)
+
+
