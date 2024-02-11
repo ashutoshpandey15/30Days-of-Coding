dd33f13a333ae3d6f596fdc16f173396cf4410a1
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2024-02-11 10:31:40 +0530

    Done with Oops in python

---
+++ Oops.py
@@ -0,0 +1,29 @@
+#Object Oriented Programming in Python
+#Class and Objects
+# Think of a railway form as a template. It has spaces for your name, age, gender, and destination.
+# When you fill out the form, you're making a specific instance of that template.
+# Your details, like your name being John Doe, age 30, gender male, and destination New York, are what make up that instance.
+# So, the form is like the class, and the filled-out form is like an object.
+
+"""class Railwayform:
+    def __init__(self,name,age,number,salary):
+        self.name = name
+        self.age = age
+        self.number = number
+        self.salary = salary
+        
+    def getSalary(self):
+        return self.salary
+
+Ashutosh = Railwayform("Ashutosh",20,123456789,200000)
+print(Ashutosh.name)  # Accessing attributes using dot notation
+print(Ashutosh.age)  # Accessing attributes using dot notation
+print(Ashutosh.number)  # Accessing attributes using dot notation
+print(Ashutosh.salary)  # Accessing attributes using dot notation
+Abhishek = Railwayform("Abhishek",18,156789,120000)
+
+print (vars(Ashutosh))  # Accessing attributes using vars function
+print (vars(Abhishek))  # Accessing attributes using vars function
+"""
+
+    
