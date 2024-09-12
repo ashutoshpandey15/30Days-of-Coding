7b2540c78496db5266834f09b0a9a26df4597828
Turbo911 <ashutosh.pandey@aissmsioit.org>
2024-09-12 21:16:08 +0530

    Basic:started

---
+++ Practice/basic_day1.js
@@ -0,0 +1,17 @@
+// beginning of the revision of js from scratch
+const ashu = "Ashu";     //value remians constant cannot be changed
+let myName = "Ashu";    //another way of declaring variable but mostly used because of scope
+var myAdd = "Address";  //most common way to declare the variable (not used cause of block and function scope)
+let myName1 ;  // variale declared but not initialized hence => undefined
+console.table({ashu, myName, myAdd , myName1}); // to consolelog in table format
+
+
+// "use strict" this treats the JS code as newwe version of JS.
+
+// Data types in js
+// Primitive data types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
