d3a6ae612ef9d47e62299efb0c271ab7cbee29d1
Turbo911 <ashutosh.pandey@aissmsioit.org>
2024-09-12 21:35:27 +0530

    Conversion of Datatypes

---
+++ Practice/conversions_01.js
@@ -0,0 +1,15 @@
+// Integer
+let score = 33;
+console.log(typeof score);
+
+// COnvert to string
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1
+ // 1 => true
+ // 0 => false
+ // "" => 0
+ // "abc" =>false
